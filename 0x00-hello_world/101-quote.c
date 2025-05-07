52751ecc8dfd6f5dd5032a7afecca8294a93cad2
Tobi-Kazim <kazimtobi1994@gmail.com>
2025-05-07 09:11:36 -0700

    Sending to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
   */
 int main(void)
 {
-	write(stderr,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+	fwrite("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stderr);
+
 	return (1);
 }
 
