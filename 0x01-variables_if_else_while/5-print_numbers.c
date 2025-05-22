d37aeefddd3ab802a1ebc90bb4ebdc083dbf6e19
Tobi-Kazim <kazimtobi1994@gmail.com>
2025-05-22 08:50:44 -0700

    Enter numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 is Success
+ *
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+		printf("\n");
+		return (0);
+}
