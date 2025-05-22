df53d86705ae7a01e4c7b0d5f6355b6698086068
Tobi-Kazim <kazimtobi1994@gmail.com>
2025-05-22 07:08:58 -0700

    Write the alphebet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 is Success
+ *
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+		putchar('\n');
+		return (0);
+}
