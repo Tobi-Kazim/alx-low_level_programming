9d6fe762a604719def8a761b49dcc2a659f4a34d
Tobi-Kazim <kazimtobi1994@gmail.com>
2025-06-18 08:27:35 +0000

    Output all combinations of triple digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: 0 is success
+ *
+ */
+int main(void)
+{
+	int i, j, k;
+	for (i = 0;  i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if ((i != 8 || j != 9) && (i != 8 || k != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
