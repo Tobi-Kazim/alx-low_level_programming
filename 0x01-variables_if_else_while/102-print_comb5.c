2422d961e6732ffa7d8447d8724fe06ad2946e10
Tobi-Kazim <kazimtobi1994@gmail.com>
2025-06-23 11:49:58 +0000

    Output all possible combinations of two 2-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry into a program
+ *
+ * Return: 0 is success
+ *
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 98; x++)
+	{
+		for (y = 0; y <= 99; y++)
+		{
+			putchar('0' + x / 10);
+			putchar('0' + x % 10);
+			putchar(' ');
+			putchar('0' + y / 10);
+			putchar('0' + y % 10);
+			if (x != 98 || y != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
